8711861ab3d82dd6af03d2735238c9f482ad46d9
Bram Schoenmakers <me@bramschoenmakers.nl>
2012-03-03 17:47:31 +0100

    Move graphing into a function.

--- arma/arma.js
+++ arma/arma.js
@@ -40,81 +40,84 @@ function arma( ar, ma, n ) {
   return result;
 }
 
-var data = arma( [1], [1], 100 );
+function update( ar, ma, n ) {
+  var data = arma( ar, ma, n );
 
-var WIDTH = 800;
-var HEIGHT = 600;
-var MARGIN = 20;
+  var WIDTH = 800;
+  var HEIGHT = 600;
+  var MARGIN = 20;
 
-y = d3.scale.linear().domain( [ d3.min( data.concat( [ 0 ] ) ), d3.max( data.concat( [ ] ) ) ] ).range( [ 0 + MARGIN, HEIGHT - MARGIN ] );
-x = d3.scale.linear().domain( [ 0, 100 ] ).range( [ 0 + MARGIN, WIDTH - MARGIN ] );
+  y = d3.scale.linear().domain( [ d3.min( data.concat( [ 0 ] ) ), d3.max( data.concat( [ ] ) ) ] ).range( [ 0 + MARGIN, HEIGHT - MARGIN ] );
+  x = d3.scale.linear().domain( [ 0, 100 ] ).range( [ 0 + MARGIN, WIDTH - MARGIN ] );
 
-var graph = d3.select( "body" )
-  .append( "svg:svg" )
-  .attr( "width", WIDTH )
-  .attr( "height", HEIGHT );
+  var graph = d3.select( "#graph" )
+    .append( "svg:svg" )
+    .attr( "width", WIDTH )
+    .attr( "height", HEIGHT );
 
-var g = graph.append( "svg:g" )
-  .attr( "transform", "translate(0,600)" );
+  var g = graph.append( "svg:g" )
+    .attr( "transform", "translate(0,600)" );
 
-var line = d3.svg.line()
-  .x( function( d, i ) { return x( i ) } )
-  .y( function( d ) { return -1 * y( d ) } );
+  var line = d3.svg.line()
+    .x( function( d, i ) { return x( i ) } )
+    .y( function( d ) { return -1 * y( d ) } );
 
-g.append( "svg:path" ).attr( "d", line( data ) );
+  g.append( "svg:path" ).attr( "d", line( data ) );
 
-// x axis
-g.append( "svg:line" )
-  .attr( "x1", x( 0 ) )
-  .attr( "y1", -1 * y( 0 ) )
-  .attr( "x2", x( WIDTH ) )
-  .attr( "y2", -1 * y( 0 ) );
+  // x axis
+  g.append( "svg:line" )
+    .attr( "x1", x( 0 ) )
+    .attr( "y1", -1 * y( 0 ) )
+    .attr( "x2", x( WIDTH ) )
+    .attr( "y2", -1 * y( 0 ) );
 
-// y axis
-g.append( "svg:line" )
-  .attr( "x1", x( 0 ) )
-  .attr( "y1", -1 * y( d3.min( data.concat( [ 0 ] ) ) ) )
-  .attr( "x2", x( 0 ) )
-  .attr( "y2", -1 * y( d3.max( data.concat( [ 0 ] ) ) ) );
+  // y axis
+  g.append( "svg:line" )
+    .attr( "x1", x( 0 ) )
+    .attr( "y1", -1 * y( d3.min( data.concat( [ 0 ] ) ) ) )
+    .attr( "x2", x( 0 ) )
+    .attr( "y2", -1 * y( d3.max( data.concat( [ 0 ] ) ) ) );
 
-// x labels
-g.selectAll( ".xLabel" )
-  .data( x.ticks( 5 ) )
-  .enter().append( "svg:text" )
-  .attr( "class", "xLabel" )
-  .text( String )
-  .attr( "x", function( d ) { return x( d ) } )
-  .attr( "y", -1 * y( -0.7 ) )
-  .attr( "text-anchor", "middle" );
+  // x labels
+  g.selectAll( ".xLabel" )
+    .data( x.ticks( 5 ) )
+    .enter().append( "svg:text" )
+    .attr( "class", "xLabel" )
+    .text( String )
+    .attr( "x", function( d ) { return x( d ) } )
+    .attr( "y", -1 * y( -0.7 ) )
+    .attr( "text-anchor", "middle" );
 
-// y labels
-g.selectAll( ".yLabel" )
-  .data( y.ticks( 5 ) )
-  .enter().append( "svg:text" )
-  .attr( "class", "yLabel" )
-  .text( String )
-  .attr( "x", 5 )
-  .attr( "y", function( d ) { return -1 * y( d ) } )
-  .attr( "text-anchor", "middle" )
-  .attr( "dy", 4 );
+  // y labels
+  g.selectAll( ".yLabel" )
+    .data( y.ticks( 5 ) )
+    .enter().append( "svg:text" )
+    .attr( "class", "yLabel" )
+    .text( String )
+    .attr( "x", 5 )
+    .attr( "y", function( d ) { return -1 * y( d ) } )
+    .attr( "text-anchor", "middle" )
+    .attr( "dy", 4 );
 
-// x ticks
-g.selectAll( ".xTicks" )
-  .data( x.ticks( 5 ) )
-  .enter().append( "svg:line" )
-  .attr( "class", "xTicks" )
-  .attr( "x1", function( d ) { return x( d ); } )
-  .attr( "y1", -1 * y( 0 ) )
-  .attr( "x2", function( d ) { return x( d ); } )
-  .attr( "y2", -1 * y( -0.3 ) );
+  // x ticks
+  g.selectAll( ".xTicks" )
+    .data( x.ticks( 5 ) )
+    .enter().append( "svg:line" )
+    .attr( "class", "xTicks" )
+    .attr( "x1", function( d ) { return x( d ); } )
+    .attr( "y1", -1 * y( 0 ) )
+    .attr( "x2", function( d ) { return x( d ); } )
+    .attr( "y2", -1 * y( -0.3 ) );
 
-// y ticks
-g.selectAll( ".yTicks" )
-  .data( y.ticks( 4 ) )
-  .enter().append( "svg:line" )
-  .attr( "class", "yTicks" )
-  .attr( "x1", x( 0 ) )
-  .attr( "y1", function( d ) { return -1 * y( d ) } )
-  .attr( "x2", x( -0.01 * 100 ) )
-  .attr( "y2", function( d ) { return -1 * y( d ) } );
+  // y ticks
+  g.selectAll( ".yTicks" )
+    .data( y.ticks( 4 ) )
+    .enter().append( "svg:line" )
+    .attr( "class", "yTicks" )
+    .attr( "x1", x( 0 ) )
+    .attr( "y1", function( d ) { return -1 * y( d ) } )
+    .attr( "x2", x( -0.01 * 100 ) )
+    .attr( "y2", function( d ) { return -1 * y( d ) } );
+}
 
+update( [ 1 ], [ 1 ], 100 );
